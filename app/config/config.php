fdb17d07fb37115f6889b051fbea349664c0a1f7
kreuzlin <33320877+kreuzlin@users.noreply.github.com>
2022-06-28 18:57:02 +0200

    Update config.php
    db name changed to sh

--- app/config/config.php
+++ app/config/config.php
@@ -3,7 +3,7 @@
   define("DB_HOST", "cyber.postgres.database.azure.com");
   define("DB_USER", "kitchen");
   define("DB_PASS", "adp7h-hj7hm-K6zik");
-  define("DB_NAME", "postgres");
+  define("DB_NAME", "sh");
 
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
