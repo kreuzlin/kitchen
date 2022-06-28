79e51dd5a1cbebd20b6b9955783fc18cee0107e1
kreuzlin <33320877+kreuzlin@users.noreply.github.com>
2022-06-28 16:59:57 +0200

    Update config.php
    domain and db config

--- app/config/config.php
+++ app/config/config.php
@@ -1,14 +1,14 @@
 <?php
   // DB Params
-  define("DB_HOST", "localhost");
-  define("DB_USER", "php");
-  define("DB_PASS", "Dist1234");
-  define("DB_NAME", "sh");
+  define("DB_HOST", "cyber.postgres.database.azure.com");
+  define("DB_USER", "kitchen");
+  define("DB_PASS", "adp7h-hj7hm-K6zik");
+  define("DB_NAME", "public");
 
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
   // URL Root
-  define('URLROOT', 'http://localhost:8080');
+  define('URLROOT', 'https://secgovcode.azurewebsites.net');
   // Site Name
   define('SITENAME', 'SecGovCode');
   
